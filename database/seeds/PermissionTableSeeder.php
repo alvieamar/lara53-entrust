d14e796dbbeb7072a9f61af25d7bbf6ed92590da
alvieamar <alvie.amar@bibo.com.ph>
2016-11-29 15:45:56 +0800

    seeder

--- database/seeds/PermissionTableSeeder.php
+++ database/seeds/PermissionTableSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Permission;
+use Carbon\Carbon;
+//use App\Permission;
 
 class PermissionTableSeeder extends Seeder
 {
@@ -16,47 +17,67 @@ class PermissionTableSeeder extends Seeder
             [
                 'name' => 'role-list',
                 'display_name' => 'Display Role Listing',
-                'description' => 'See only Listing Of Role'
+                'description' => 'See only Listing Of Role',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
             [
                 'name' => 'role-create',
                 'display_name' => 'Create Role',
-                'description' => 'Create New Role'
+                'description' => 'Create New Role',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
             [
                 'name' => 'role-edit',
                 'display_name' => 'Edit Role',
-                'description' => 'Edit Role'
+                'description' => 'Edit Role',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
             [
                 'name' => 'role-delete',
                 'display_name' => 'Delete Role',
-                'description' => 'Delete Role'
+                'description' => 'Delete Role',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
             [
                 'name' => 'item-list',
                 'display_name' => 'Display Item Listing',
-                'description' => 'See only Listing Of Item'
+                'description' => 'See only Listing Of Item',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
             [
                 'name' => 'item-create',
                 'display_name' => 'Create Item',
-                'description' => 'Create New Item'
+                'description' => 'Create New Item',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
             [
                 'name' => 'item-edit',
                 'display_name' => 'Edit Item',
-                'description' => 'Edit Item'
+                'description' => 'Edit Item',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
             [
                 'name' => 'item-delete',
                 'display_name' => 'Delete Item',
-                'description' => 'Delete Item'
+                'description' => 'Delete Item',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ]
         ];
 
         foreach ($permission as $key => $value) {
-            Permission::create($value);
+//            App\Permission::create($value);
+
+            DB::table('permissions')->insert([
+                $key => $value,
+            ]);
         }
     }
 }
